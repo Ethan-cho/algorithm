a4add126868f50a2d63929153ff4cb161e322184
Ethan-cho <akswocho@naver.com>
2018-01-27 12:20:02 +0900

    Update 탈주범recursive.cpp

--- 탈주범recursive.cpp
+++ 탈주범recursive.cpp
@@ -15,23 +15,23 @@ void init(){
     }
 }
 
-void solve(int row,int col,int time){
-	if(time>l)	return;	//시간 초과시 종료
+void solve(int row,int col,int hour){
+	if(hour>l)	return;
 	
-	
-	visit[row][col]=true;	//방문여부체크
-	cnt[row][col]++;		//방문횟수증가
+	int ni,nj;
+	visit[row][col]=true;
+	cnt[row][col]++;
 	
 	if(pipe[map[row][col]][0]==1){	//up
 		//row-1, col
 		int k=0;	//up
 		int opp_k=1;	//down
-		if(map[row][col]!=0){	//현재 파이프가 갈 수 있는가
-			if(pipe[map[row][col]][0]==pipe[map[row][col]][1]){	//진행가능여부 k와 opposite k
+		if(map[row-1][col]!=0){		//현재 파이프가 갈 수 있는가
+			if(pipe[map[row][col]][0]==pipe[map[row-1][col]][1]){	//진행가능여부 k와 opposite k
 				if(0<=row-1&&row-1<n&&0<=col&&col<m){	//safe한 범위
 					if(visit[row-1][col]==false){	//방문안했다면
-						solve(row-1,col,time+1);	//방문
-					}
+						solve(row-1,col,hour+1);	//방문
+					}	
 				}
 			}
 		}
@@ -41,11 +41,11 @@ void solve(int row,int col,int time){
 		//row+1, col
 		int k=1;
 		int opp_k=0;
-		if(map[row][col]!=0){
+		if(map[row+1][col]!=0){
 			if(pipe[map[row][col]][1]==pipe[map[row+1][col]][0]){
 				if(0<=row+1&&row+1<n&&0<=col&&col<m){
 					if(visit[row+1][col]==false){
-						solve(row+1,col,time+1);
+						solve(row+1,col,hour+1);
 					}
 				}
 			}
@@ -58,9 +58,9 @@ void solve(int row,int col,int time){
 		int opp_k=3;
 		if(map[row][col-1]!=0){
 			if(pipe[map[row][col]][2]==pipe[map[row][col-1]][3]){
-				if(0<=row&&col<n&&0<=row-1&&col-1<m){
+				if(0<=row&&row<n&&0<=col-1&&col-1<m){
 					if(visit[row][col-1]==false){
-						solve(row,col-1,time+1);
+						solve(row,col-1,hour+1);
 					}
 				}
 			}
@@ -75,7 +75,7 @@ void solve(int row,int col,int time){
 			if(pipe[map[row][col]][3]==pipe[map[row][col+1]][2]){
 				if(0<=row&&row<n&&0<=col+1&&col+1<m){
 					if(visit[row][col+1]==false){
-						solve(row,col+1,time+1);
+						solve(row,col+1,hour+1);
 					}
 				}
 			}
